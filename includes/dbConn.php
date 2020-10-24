94a71525c5c21a525de65a4a5b2c6be8424d9209
lhy2016 <lhy920104@gmail.com>
2020-10-24 19:02:26 +0800

    Update dbConn.php

--- includes/dbConn.php
+++ includes/dbConn.php
@@ -1,37 +1,10 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "lhy_2016";
 $db_name = "company";
-phpinfo();
-function mysqlinstalled (){
-    if (function_exists ("mysql_connect")){
-     return true;
-    } else {
-     return false;
-    }
-   }
-   function mysqliinstalled (){
-    if (function_exists ("mysqli_connect")){
-     return true;
-    } else {
-     return false;
-    }
-   }
-    
-   if (mysqlinstalled()){
-    echo "<p>The mysql extension is installed.</p>";
-   } else {
-    echo "<p>The mysql extension is not installed..</p>";
-   }
-   if (mysqliinstalled()){
-    echo "<p>The mysqli extension is installed.</p>";
-   } else {
-    echo "<p>The mysqli extension is not installed..</p>";
-   }
 
 $conn = new mysqli($servername,$username,$password,$db_name);
-print_r($conn);
 if ($conn->connect_error) {
     echo($conn->connect_error);
     die("Connection failed: " . $conn->connect_error);
