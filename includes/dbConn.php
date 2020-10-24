48592f762e2bb96674c7c9649f29358bffb4770f
lhy2016 <lhy920104@gmail.com>
2020-10-24 18:25:00 +0800

    Update dbConn.php

--- includes/dbConn.php
+++ includes/dbConn.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $db_name = "company";
- 
+phpinfo();
 function mysqlinstalled (){
     if (function_exists ("mysql_connect")){
      return true;
