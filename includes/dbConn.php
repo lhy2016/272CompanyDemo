446fe1b74c27ae5937efcfc345e24248cbe07f99
lhy2016 <lhy920104@gmail.com>
2020-10-24 17:53:33 +0800

    test
    test

--- includes/dbConn.php
+++ includes/dbConn.php
@@ -1,9 +1,35 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "lhy_2016";
+$password = "";
 $db_name = "company";
-phpinfo();
+ 
+function mysqlinstalled (){
+    if (function_exists ("mysql_connect")){
+     return true;
+    } else {
+     return false;
+    }
+   }
+   function mysqliinstalled (){
+    if (function_exists ("mysqli_connect")){
+     return true;
+    } else {
+     return false;
+    }
+   }
+    
+   if (mysqlinstalled()){
+    echo "<p>The mysql extension is installed.</p>";
+   } else {
+    echo "<p>The mysql extension is not installed..</p>";
+   }
+   if (mysqliinstalled()){
+    echo "<p>The mysqli extension is installed.</p>";
+   } else {
+    echo "<p>The mysqli extension is not installed..</p>";
+   }
+
 $conn = new mysqli($servername,$username,$password,$db_name);
 if ($conn->connect_error) {
     echo($conn->connect_error);
