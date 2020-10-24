1a8b64c88e43023495ece1add63b5216d9380e36
lhy2016 <lhy920104@gmail.com>
2020-10-24 17:48:24 +0800

    test php info
    test phpinfo

--- includes/dbConn.php
+++ includes/dbConn.php
@@ -3,8 +3,10 @@ $servername = "localhost";
 $username = "root";
 $password = "lhy_2016";
 $db_name = "company";
+phpinfo();
 $conn = new mysqli($servername,$username,$password,$db_name);
 if ($conn->connect_error) {
+    echo($conn->connect_error);
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
